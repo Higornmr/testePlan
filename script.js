6e45680c3593157cee0a4ff426188c7e0ba15a18
Higor Rodrigues <higor_1992@hotmail.com>
2023-03-05 11:01:39 -0300

    Trabalhando na responsividade do site e mudando alguns estilos para deixar o site mais responsivel

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const header = document.getElementById("header");
 const menu = document.getElementById("navbar");
 const hamburger = document.getElementById("hamburger");
+const carousel = document.querySelectorAll(".flex-cols-wrap");
 
 hamburger.addEventListener("click", () => {
-    header.classList.toggle("menu-active");
+  header.classList.toggle("menu-active");
 });
 
-document.querySelectorAll("#navbar a").forEach(anchor => {
-    anchor.addEventListener('click', () => {        
-        header.classList.remove("menu-active");
-    });
-}); 
+document.querySelectorAll("#navbar a").forEach((anchor) => {
+  anchor.addEventListener("click", () => {
+    header.classList.remove("menu-active");
+  });
+});
+
+function handleResize() {
+  if (
+    carousel &&
+    window.innerWidth < 900 &&
+    carousel.classList.contains("carousel--mobile")
+  ) {
+    carousel.classList.remove("carousel--mobile");
+  } else if (
+    carousel &&
+    window.innerWidth >= 900 &&
+    !carousel.classList.contains("carousel--mobile")
+  ) {
+    carousel.classList.add("carousel--mobile");
+  }
+}
+
+window.addEventListener("resize", handleResize);
+handleResize();
